1cbec7670d1fcbdae6ea67f53e6159c26bc944a4
Fasih Qureshi <fasihqureshi@Fasihs-MacBook-Air.local>
2017-11-18 17:05:13 +0000

    I have added a custom font called Nilland and have used the font as the labels. The custom font was declared in the info.plist file I am using the colour format that I referred to in the Documentation. I have a white/cream background with red text. I have declared variables in ViewController.h (first view controller) I have also used an open source logo in my startup and this will form the basis of my app

--- BrainAppQuiz/ViewController.h
+++ BrainAppQuiz/ViewController.h
@@ -9,7 +9,22 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+// In this section we declare the permanent variables
+#pragma mark Permanent Variables
 
+@property (weak, nonatomic) IBOutlet UILabel *OneStepQuiztitle;
+@property (weak, nonatomic) IBOutlet UILabel *welcomelabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *Imageoflogo;
+@property (weak, nonatomic) IBOutlet UILabel *ClickonButtontobegin;
+
+// Here we have outlet and action for the button
+
+#pragma mark Button Variables
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonaction;
+
+- (IBAction)action1button:(UIButton *)sender;
 
 @end
 
--- BrainAppQuiz/ViewController.m
+++ BrainAppQuiz/ViewController.m
@@ -26,4 +26,6 @@
 }
 
 
+- (IBAction)action1button:(UIButton *)sender {
+}
 @end
